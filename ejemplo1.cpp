f77cfbccefa04afbb208ca22f740f0e199873e48
Jonathan-05022007 <chamaledelcidjonathaneladio@gmail.com>
2026-01-28 17:13:36 -0600

    Add files via upload

---
+++ ejemplo1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"Hello world!" << endl;
+    cout <<"Jonatha Eladio Chamale Del cid" <<endl;
+    cout <<"9959-25-7858" <<endl;
+    return 0;
+}
